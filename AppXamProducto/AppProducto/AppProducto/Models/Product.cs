6a4a1396d205d22db701dac3a39c32cb46cc16f0
Mitch <Yerko@DESKTOP-L1AAA5D>
2020-05-14 18:49:57 -0400

    Progreso 14/5/2020

---
+++ AppXamProducto/AppProducto/AppProducto/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppProducto.Models
+{
+    public class Product
+    {
+        [JsonProperty(PropertyName = "productid")]
+        public int ProductId { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "value")]
+        public double Value { get; set; }
+    }
+}
